5e9114da7b4d8ecfac7434c12e74a410467fbeb9
Dunyadj <dunya.jadallah18@gmail.com>
2024-05-21 18:04:46 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -14,3 +14,43 @@ prattinstmarker.bindPopup(`
     <b>Pratt Institute</b><br>Sculpture Garden and green space <br>
     <img src="https://aurashktest.github.io/aurashktest/images/pratt.jpg" style="width: 100px; height: auto;">
 `).openPopup();
+
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          [
+            -74.02824166226117,
+            40.622933042455315
+          ],
+          [
+            -74.02555499328983,
+            40.62945819788405
+          ]
+        ],
+        "type": "LineString"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          [
+            -74.03586668467536,
+            40.63334191629062
+          ],
+          [
+            -74.02509442146625,
+            40.630856362532114
+          ]
+        ],
+        "type": "LineString"
+      }
+    }
+  ]
+}
