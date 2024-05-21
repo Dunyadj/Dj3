0c1e3b5e83ec7a080506d067b65e03593da033d1
Dunyadj <dunya.jadallah18@gmail.com>
2024-05-21 17:45:57 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -10,7 +10,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var Tanoreenmarker = L.marker([40.63141267176866, -74.02759977518336]).addTo(map);
 
 // Add a popup to the marker
-marker.bindpopup.("Tanoreen</b><br>This is a leaflet
+marker.bindpopup.("Tanoreen</b><br>This is a leaflet map.").openpopup();
     <b>Tanoreen</b><br>Best Arabic Food in Bay Ridge<br>
     <img src="/dunyadj.github.io/Dj3/tanoreen-brand.png" style="width: 100px; height: auto;">
 `).openPopup();
