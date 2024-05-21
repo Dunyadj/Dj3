98169cf43ddb8226a0b3af314f2810893634b65b
Dunyadj <dunya.jadallah18@gmail.com>
2024-05-21 19:38:39 -0400

    Update your_name24.js

--- your_name24.js
+++ your_name24.js
@@ -36,21 +36,3 @@ fetch('https://Dj3.github.io/Dj3/dunyadj_daytimeroute.geojson')
         console.error('Error loading GeoJSON file:', error);
     });
 
-
-    // Load the GeoJSON line file
-fetch('https://aurashktest.github.io/aurashktest/akeveningroute.geojson')
-.then(response => response.json())
-.then(geojson => {
-    // Customize the style of the line
-    var lineStyle = {
-        color: 'blue', // Change color as needed
-        weight: 5, // Change weight as needed
-        opacity: 0.7 // Change opacity as needed
-    };
-
-
-
-
-
-
-
